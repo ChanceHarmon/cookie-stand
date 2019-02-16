c1a22fd22c661c66789386644c327ae54b8e2caa
Chance Harmon <henrikillsit@gmail.com>
2019-02-16 11:23:42 -0800

    All five generated

--- js/app.js
+++ js/app.js
@@ -21,8 +21,8 @@ var hours = [
 var pikeUl = document.getElementById('pike');
 var seaTacUl = document.getElementById('airport');
 var seattleUl = document.getElementById('seattle');
-var hill = document.getElementById('capHill');
-var alki = document.getElementById('beach');
+var hillUl = document.getElementById('capHill');
+var alkiUl = document.getElementById('beach');
 
 
 
@@ -50,4 +50,101 @@ var pike = {
 }
   pike.render ();
 
+
+  var seaTac = {
+    name: "Seatac Airport",
+    avg : 1.2,
+    min : 3,
+    max : 24,
+    customerTotal : 0,
+    getRandom: function () {
+      var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
+      var totalCookies = Math.floor (randomNum * this.avg);
+      return totalCookies;
+    },
+  
+    render: function () {
+      for (var i = 0; i < hours.length; i++){
+        var liEl = document.createElement('li');
+        this.customerTotal = this.getRandom();
+        liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
+        seaTacUl.appendChild(liEl);
+      }
+  
+    }
+  }
+    seaTac.render ();
+
+    var seattle = {
+      name: "Seattle Center",
+      avg : 3.7,
+      min : 11,
+      max : 38,
+      customerTotal : 0,
+      getRandom: function () {
+        var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
+        var totalCookies = Math.floor (randomNum * this.avg);
+        return totalCookies;
+      },
+    
+      render: function () {
+        for (var i = 0; i < hours.length; i++){
+          var liEl = document.createElement('li');
+          this.customerTotal = this.getRandom();
+          liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
+          seattleUl.appendChild(liEl);
+        }
+    
+      }
+    }
+      seattle.render ();
+
+      var hill = {
+        name: "Capitol Hill",
+        avg : 2.3,
+        min : 20,
+        max : 38,
+        customerTotal : 0,
+        getRandom: function () {
+          var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
+          var totalCookies = Math.floor (randomNum * this.avg);
+          return totalCookies;
+        },
+      
+        render: function () {
+          for (var i = 0; i < hours.length; i++){
+            var liEl = document.createElement('li');
+            this.customerTotal = this.getRandom();
+            liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
+            hillUl.appendChild(liEl);
+          }
+      
+        }
+      }
+        hill.render ();
+
+
+        var alki = {
+          name: "Alki",
+          avg : 4.6,
+          min : 2,
+          max : 16,
+          customerTotal : 0,
+          getRandom: function () {
+            var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
+            var totalCookies = Math.floor (randomNum * this.avg);
+            return totalCookies;
+          },
+        
+          render: function () {
+            for (var i = 0; i < hours.length; i++){
+              var liEl = document.createElement('li');
+              this.customerTotal = this.getRandom();
+              liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
+              alkiUl.appendChild(liEl);
+            }
+        
+          }
+        }
+          alki.render ();
 
