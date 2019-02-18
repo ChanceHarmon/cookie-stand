a4f443e698b80b49f1a18ff40059c5a72dd064d3
Chance Harmon <henrikillsit@gmail.com>
2019-02-17 22:08:07 -0800

    Not Happy

--- js/app.js
+++ js/app.js
@@ -1,150 +1,44 @@
 'use strict'
 console.log('script loaded');
-var hours = [
-  '6am',
-  '7am',
-  '8am',
-  '9am',
-  '10am',
-  '11am',
-  '12pm',
-  '1pm',
-  '2pm',
-  '3pm',
-  '4pm',
-  '5pm',
-  '6pm',
-  '7pm',
-  '8pm',
-];
 
-var pikeUl = document.getElementById('pike');
-var seaTacUl = document.getElementById('airport');
-var seattleUl = document.getElementById('seattle');
-var hillUl = document.getElementById('capHill');
-var alkiUl = document.getElementById('beach');
-
-
+getRandom () = 
+  randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
+  var totalCookies = Math.floor (randomNum * this.avg);
+  return totalCookies;
 
 var pike = {
   name: "1st and Pike",
   avg : 6.3,
   min : 23,
   max : 65,
-  customerTotal : 0,
-  getRandom: function () {
-    var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
-    var totalCookies = Math.floor (randomNum * this.avg);
-    return totalCookies;
-  },
-
-  render: function () {
-    for (var i = 0; i < hours.length; i++){
-      var liEl = document.createElement('li');
-      this.customerTotal = this.getRandom();
-      liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
-      pikeUl.appendChild(liEl);
-    }
-
-  }
-}
-  pike.render ();
-
-
-  var seaTac = {
+  customerTotal : 0}
+  
+var seaTac = {
     name: "Seatac Airport",
     avg : 1.2,
     min : 3,
     max : 24,
-    customerTotal : 0,
-    getRandom: function () {
-      var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
-      var totalCookies = Math.floor (randomNum * this.avg);
-      return totalCookies;
-    },
-  
-    render: function () {
-      for (var i = 0; i < hours.length; i++){
-        var liEl = document.createElement('li');
-        this.customerTotal = this.getRandom();
-        liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
-        seaTacUl.appendChild(liEl);
-      }
-  
-    }
-  }
-    seaTac.render ();
+    customerTotal : 0}
 
-    var seattle = {
+var seattle = {
       name: "Seattle Center",
       avg : 3.7,
       min : 11,
       max : 38,
-      customerTotal : 0,
-      getRandom: function () {
-        var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
-        var totalCookies = Math.floor (randomNum * this.avg);
-        return totalCookies;
-      },
+      customerTotal : 0}
     
-      render: function () {
-        for (var i = 0; i < hours.length; i++){
-          var liEl = document.createElement('li');
-          this.customerTotal = this.getRandom();
-          liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
-          seattleUl.appendChild(liEl);
-        }
-    
-      }
-    }
-      seattle.render ();
-
-      var hill = {
+ var hill = {
         name: "Capitol Hill",
         avg : 2.3,
         min : 20,
         max : 38,
-        customerTotal : 0,
-        getRandom: function () {
-          var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
-          var totalCookies = Math.floor (randomNum * this.avg);
-          return totalCookies;
-        },
-      
-        render: function () {
-          for (var i = 0; i < hours.length; i++){
-            var liEl = document.createElement('li');
-            this.customerTotal = this.getRandom();
-            liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
-            hillUl.appendChild(liEl);
-          }
-      
-        }
-      }
-        hill.render ();
-
-
-        var alki = {
+        customerTotal : 0}
+       
+ var alki = {
           name: "Alki",
           avg : 4.6,
           min : 2,
           max : 16,
-          customerTotal : 0,
-          getRandom: function () {
-            var randomNum = Math.floor (Math.random() * (this.max - this.min))+this.min;
-            var totalCookies = Math.floor (randomNum * this.avg);
-            return totalCookies;
-          },
-        
-          render: function () {
-            for (var i = 0; i < hours.length; i++){
-              var liEl = document.createElement('li');
-              this.customerTotal = this.getRandom();
-              liEl.textContent = `${hours[i]} : ${this.customerTotal} cookies per hour.`;
-              alkiUl.appendChild(liEl);
-            }
-        
-          }
-        }
-          alki.render ();
-
+          customerTotal : 0
+ }
+ getRandom(alki);
